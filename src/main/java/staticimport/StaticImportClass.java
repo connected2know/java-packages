0097f1bd299a2473c3bb73f41bd66a6b4f1ce7f0
tpinho <tiaho_pinho@optum.com>
2019-11-03 12:45:39 +0000

    static import example

--- src/main/java/staticimport/StaticImportClass.java
+++ src/main/java/staticimport/StaticImportClass.java
@@ -4,7 +4,7 @@ import static main.java.staticimport.StaticMethodClass.staticmethod;
 
 public class StaticImportClass {
 
-	void getStudentName() {
+	void callStaticMethod() {
 
 		staticmethod(); // It's possible to call the static method directly after doing a static import
 
